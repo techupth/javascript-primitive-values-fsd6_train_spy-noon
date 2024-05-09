9484770500d6a7db4dc25e1022b23e6eca71509d
Spy <sornram.spy@gmail.com>
2024-05-09 14:28:11 +0700

    feat: Js

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let sumPrice = 27000
+let discountPrices = sumPrice * 25 / 100
+let priceBill = sumPrice - discountPrices
+console.log(priceBill)
